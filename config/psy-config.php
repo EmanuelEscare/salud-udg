99977c0b078858c999b1af379aed139c03021338
EmanuelEsc <emanuel.escareno@gvadeto.com>
2025-11-04 22:57:54 -0600

    Remove psy config from gitignore

---
+++ config/psy-config.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+  'folio' => '2025B',
+  'monday' => true,
+  'tuesday' => true,
+  'wednesday' => true,
+  'thursday' => true,
+  'friday' => true,
+  'saturday' => true,
+  'sunday' => false,
+  'startMorning' => '08:00',
+  'endMorning' => '14:00',
+  'startAfternoon' => '16:00',
+  'endAfternoon' => '20:00',
+  'Domingo' => false,
+  'Sábado' => false,
+);
